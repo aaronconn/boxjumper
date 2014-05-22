bf0f3f7ec4c31d5a4ac48d9520461e1fa89439ab
aaronconn <aaron.r.conn@gmail.com>
2014-05-22 15:47:00 -0500

    Create Level.java

---
+++ Level.java
@@ -0,0 +1,73 @@
+/**
+ * Level.java  12/5/2014
+ *
+ * @author - Aaron Conn
+ * @author - Chris Best
+ * @author - Preeti Singh
+ * @version 1.00 13/5/2014
+ *
+ */
+ 
+package boxjumper;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.util.ArrayList;
+
+public class Level extends JPanel implements ActionListener
+{
+	private Timer timer;
+	private Player player; 
+	
+    public Level()
+    {
+    	addKeyListener(new TAdapter());
+    	setFocusable(true);
+    	setBackground(new Color(0,191,255));   //sky blue
+    	setDoubleBuffered(true);
+    	
+    	player = new Player();
+    	
+    	timer = new Timer(5,this);
+    	timer.start();
+    }
+    
+    public void paint(Graphics window)
+    {
+        super.paint(window);
+
+        Graphics2D g2d = (Graphics2D)window;
+        g2d.drawImage(player.getImage(), player.getX(), player.getY(), this);
+
+        Toolkit.getDefaultToolkit().sync();
+        window.dispose();
+    }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        player.move();
+        repaint();  
+    }
+    
+    private class TAdapter extends KeyAdapter
+    {
+
+        public void keyReleased(KeyEvent e)
+        {
+            player.keyReleased(e);
+        }
+
+        public void keyPressed(KeyEvent e)
+        {
+            player.keyPressed(e);
+        }
+    }
+}
